08d6eafb90e487535561a071040761dc251af229
00014055 <wiut00014055@gmail.com>
2023-03-27 01:44:06 +0500

    .gitignore

--- app.js
+++ app.js
@@ -2,25 +2,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const fs = require('fs')
+//const fs = require('fs')
 
 app.set('view engine', 'pug')
-app.use(express.urlencodded({exttended:false}))
+//app.use(express.urlencodded({exttended:false}))
 
+
+// http://localhost:3000
 app.get('/', (req, res) => {
-  let clients = JSON.parse(fs.readFileSync(./data/clients/JSON))
+	res.render('index')
+  //let clients = JSON.parse(fs.readFileSync(./data/clients/JSON))
 
-  res.send('list', {clients:clients})
+  //res.send('list', {clients:clients})
 })
 
-app.get('/add',(req, res) => {
-	res.render('form')
-})
+//app.get('/add',(req, res) => {
+//	res.render('form')
+//})
 
-app.('/add',(req, res) => {
-	res.render('form')
-})
+//app.('/add',(req, res) => {
+//	res.render('form')
+//})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(port, (err) => {
+	if (err) throw err
+
+  	console.log(`Example app listening on port ${port}`)
 })
