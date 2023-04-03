ae3f7fd418e5339118a770d9a09e7847339ce350
00014055 <wiut00014055@gmail.com>
2023-04-03 11:22:16 +0500

    first commit

--- README.md
+++ README.md
@@ -1,19 +1,32 @@
-## UzWi 
-WIKI application for finding information about Uzbek celebrities
+## About the Application 
+The application is a blog application where users can add, review, delete and update blog posts.
  
-## Installation
+### Installation
+To run the application locally, follow these steps:
 
-Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+1. Clone the repository from [GitHub](https://github.com/00014055/node)
+2. Install dependencies using the following command:
+
 
 ```bash
-pip install foobar
+npm install
 ```
- install the app and libraries: *express* and *pug*
+3. Start the application using the following command:
+```bash
+npm start
+```
+4. Open your web browser and go to http://localhost:3000 to view the application.
 
 ### Dependencies
-- Express
-- Pug
 
-link to the public repo : [GitHub](https://github.com/00014055/node)
-link to the hosted application : [Glitch](https://glitch.com/)
+The application is built using Node.js as a base web framework and uses the following third-party libraries:
+
+- Express.js (for routing)
+- Pug (for template rendering)
+- Express-validator (for input validation)
+
+
+### Public Repo and Hosted Application
+Link to the public repo: [GitHub](https://github.com/00014055/node)
+Link to the hosted application: [Glitch](https://glitch.com/)
 
--- app.js
+++ app.js
@@ -1,32 +1,159 @@
+//CRUD operation and validation
+const fs = require('fs')
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-//const fs = require('fs')
+const path = "./data/animes.json"
 
-app.set('view engine', 'pug') 
+const id = require('uniqid')
+
+const { check, validationResult } = require('express-validator'); 
+
+app.set('view engine', 'pug')
 app.use('/static', express.static('public/styles'))
-//app.use(express.urlencodded({exttended:false}))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.static('public'))
 
-
-// http://localhost:3000
 app.get('/', (req, res) => {
-	res.render('index')
-  //let clients = JSON.parse(fs.readFileSync(./data/clients/JSON))
+	let created = req.query.created
 
-  //res.send('list', {clients:clients})
+	let animes = getAll('animes')
+
+	if (created) {
+		res.render('list', {created:true, animes:animes })
+	} else {
+		res.render('list', {created:false, animes:animes})
+	} 
 })
 
-//app.get('/add',(req, res) => {
-//	res.render('form')
-//})
 
-//app.('/add',(req, res) => {
-//	res.render('form')
-//})
-
-app.listen(port, (err) => {
-	if (err) throw err
-
-  	console.log(`Example app listening on port ${port}`)
+app.get('/create', (req, res) => {
+ 	res.render('create', {})
 })
+
+//validation function was included here, but error "Cannot read properties of undefined (reading 'trim')" occured
+app.post('/create', 
+	check('name', 'must be at least 5 chars long' ).isLength({ min: 5 }),
+	check('date')
+		.isLength({ min: 5 })
+		.withMessage('must be at least 5 chars long') ,
+	check('type')
+		.isLength({ min: 5 })
+		.withMessage('must be at least 5 chars long') ,
+	check('genres')
+		.isLength({ min: 5 })
+		.withMessage('must be at least 5 chars long') ,
+	check('episodes')
+		.isLength({ min: 5 })
+		.withMessage('must be at least 5 chars long') ,
+	check('comment')
+		.isLength({ min: 10 })
+		.withMessage('must be at least 10 chars long')  ,
+	(req, res) => {
+	let data = req.body
+
+	// if (data.anime.trim() == '') {
+	// 	fs.readFile('./data/animes.json', (err, data) => {
+	// 		if (err) throw err
+
+	// 		const animes = JSON.parse(data)
+
+	// 		res.render('list', { error: true, animes:animes})
+	// })
+
+	let anime = {
+		id: id(),
+		name: data.name,
+		date: data.date,
+		type: data.type,
+		genres: data.genres,
+		episodes: data.episodes,
+		comment: data.comment,
+	}
+
+
+	const errors = validationResult(req);
+
+	let animes = getAll('animes')
+
+	animes.push(anime)
+
+	writeAll('animes', animes)
+
+	res.redirect('/?created=true')
+})
+
+
+
+app.get('/update', (req, res) => {
+ 	res.render('/update', {})
+})
+
+
+// app.get('/:id/update', (req, res) => {
+// 	const id = req.params.id
+
+// 	fs.readFile('./data/animes.json', (err, data) => {
+// 		if (err) throw err
+
+
+// 		const animes = JSON.parse(data)
+// 		const anime = animes.filter(anime => anime.id == id) [0]
+// 		const animeIdx = animes.indexOf(anime)
+// 		const splicedAnime = animes.splice(animeIdx, 1)[0]
+
+// 		splicedAnime.watched = true
+
+// 		animes.push(splicedAnime)
+
+// 		fs.writeFile('./data/animes.json', JSON.stringify(animes), (err) => {
+// 			if (err) throw err
+
+// 			res.render('list', { animes: animes })
+// 		})
+
+			
+
+// 	})
+// })
+
+
+
+//delete
+app.get('/:id', (req, res) => {
+	const id = req.params.id
+
+	fs.readFile('./data/animes.json', (err, data) => {
+		if (err) throw err
+
+		const animes = JSON.parse(data)
+
+		const filteredAnimes = animes.filter(anime => anime.id != id)
+
+		fs.writeFile('./data/animes.json', JSON.stringify(filteredAnimes), (err) => {
+			if (err) throw err
+
+			res.render('list', { animes: filteredAnimes, deleted: true })
+
+		})
+	})
+})
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+function getAll(filename) {
+	return JSON.parse(fs.readFileSync(path))
+}
+
+function writeAll(filename, data) {
+	return fs.writeFileSync(path, JSON.stringify(data))
+}
